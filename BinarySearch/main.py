cc509e4b43a61d3b5c54e6d5ab254a1d23fb3362
arsh <arshia.alidosti.72@gmail.com>
2023-06-04 23:16:36 -0700

    Added the content.

---
+++ BinarySearch/main.py
@@ -0,0 +1,19 @@
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
+
+if __name__ == "__main__":
+    print(binary_search(range(5), 3))
+    print(binary_search(range(3), 3))
